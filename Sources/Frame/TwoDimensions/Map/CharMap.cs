6ea33c8a6a33ba4df4ddb1669d9fb37e3e3c43ea
Elayan <blue-violin@hotmail.fr>
2024-12-04 10:22:36 +0100

    CharMap ctor using string lines

--- Sources/Frame/TwoDimensions/Map/CharMap.cs
+++ Sources/Frame/TwoDimensions/Map/CharMap.cs
@@ -17,6 +17,15 @@ namespace AoCTools.Frame.TwoDimensions.Map
         {
         }
 
+        /// <summary>
+        /// Creates a Map which cells each contain a char.
+        /// </summary>
+        /// <param name="mapLines">Chars in cells.</param>
+        /// <exception cref="InvalidParameterException"/>
+        public CharMap(string[] mapLines) : this(mapLines.Select(l => l.Select(c => c).ToArray()).ToArray())
+        {
+        }
+
         /// <inheritdoc/>
         protected override string LogTitle => "=== MAP ===";
     }
